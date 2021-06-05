9ac64e2a3f38d25973b45d0878a2cb9dff6f2fc5
deola <deola.c.dsilva3@gmail.com>
2021-06-06 00:24:22 +0400

    JS - color and image slider

---
+++ js/starbucks.js
@@ -0,0 +1,7 @@
+function imgSlider(pic){
+    document.querySelector('.starbucks').src = pic;
+}
+function changeCircleColor(color){
+    var circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
